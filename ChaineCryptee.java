b558fd8b29130101cff65a204a4f9940877d025c
KETAM DJILLALi <ketam.djalal@gmail.com>
2017-10-02 15:59:56 +0200

    ajout de crypte

--- ChaineCryptee.java
+++ ChaineCryptee.java
@@ -7,19 +7,17 @@
  */
 public class ChaineCryptee
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
+    
     /**
      * Constructor for objects of class ChaineCryptee
      */
-    private int declage;
+    private int decalage;
     private String enClair;
     public ChaineCryptee(String enCLair,int declage)
     {
         // initialise instance variables
         this.enClair=enClair;
-        this.declage=declage;
+        this.decalage=decalage;
     }
 
     /**
@@ -28,14 +26,29 @@ public class ChaineCryptee
      * @param  y   a sample parameter for a method
      * @return     the sum of x and y 
      */
-    public String decrypte(){
-       return enClair;
-    }
-    private static char decaleCarctere(char c, int declage){
-       return (c < 'A' || c > 'Z')? c:(char)(((c= 'A'+ decalage)%26)+ 'A');
+   
+
+   
+    public String Decrypte()
+    {
+        return enClair;
     }
     
-    
-       
+    public static char decaleCaractere ( char c , int decalage ) {
+     return (c < 'A' || c > 'Z' ) ? c : (char) ((( c - 'A' + decalage)%26 ) + 'A');
+    }
+
+    public String Crypte()
+    {
+        String ch = "";
+        int i;
+        char c;
+        for(i=0;i<enClair.length();i++)
+           { c=decaleCaractere(enClair.charAt(i),this.decalage); 
+             ch += c;}
+        return ch;
+    }
 }
+       
+
 
